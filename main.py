83845c7fae371099f134243bce784aeffa7f0650
Jack Jackson <jackjack@amazon.com>
2017-04-04 21:55:39 -0700

    Replies are now threaded

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def lambda_handler(event, context):
     body = loads(event['body'])
     text = body['event']['text']
     channel = body['event']['channel']
-    print 'Found: ' + str(text) + ' in channel ' + channel
+    message_id = body['event']['ts']
+    print 'Found: ' + str(text) + ' in channel ' + channel + ' with message_id ' + message_id
     matches = REGEX.findall(text)
 
     if matches:
@@ -27,7 +28,7 @@ def lambda_handler(event, context):
             for card in ALL_CARDS:
                 if all([c in card['title'].lower() for c in match.lower().split()]):
                     print 'all condition was matched'
-                    submitResponse(card['code'], card['title'], channel)
+                    submitResponse(card['code'], card['title'], channel, message_id)
                     break
     else:
         print 'matches was None'
@@ -37,11 +38,11 @@ def lambda_handler(event, context):
         "body": 'I hear ya'
     }
     
-def submitResponse(card_id, card_name, channel):
+def submitResponse(card_id, card_name, channel, original_message_id):
     print 'inside submitResponse: ' + str(card_id) + ':' + str(card_name) + ':' + str(channel)
     url = 'https://slack.com/api/chat.postMessage'
     
-    data = 'token=' + os.environ['responseToken'] + '&attachments=%5B%7B%27image_url%27%3A+%27https%3A%2F%2Fnetrunnerdb.com%2Fcard_image%2F' + card_id + '.png%27%2C+%27title%27%3A+%27' + card_name + '%27%7D%5D&channel=' + channel
+    data = 'token=' + os.environ['responseToken'] + '&attachments=%5B%7B%27image_url%27%3A+%27https%3A%2F%2Fnetrunnerdb.com%2Fcard_image%2F' + card_id + '.png%27%2C+%27title%27%3A+%27' + card_name + '%27%7D%5D&channel=' + channel + '&thread_ts=' + original_message_id
     print 'data: ' + str(data)
     req = urllib2.Request(url, data)
     urllib2.urlopen(req)
